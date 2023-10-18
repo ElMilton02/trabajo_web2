31c7b4a9386be1828118c1a94af7db8125a8eb15
Milton Palucci <miltonpalucci02@gmail.com>
2023-10-17 21:32:12 -0300

    se agregaron todos los templates y se modifico el ruter

--- router.php
+++ router.php
@@ -1,5 +1,50 @@
 <?php
+require_once 'apps/controllers/HomeController.php';
 
+require_once 'apps/controllers/AuthController.php';
+require_once 'apps/controllers/AboutController.php';
 
+define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
 
-?>
+$action = 'home'; // accion por defecto
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+}
+
+$params = explode('/', $action);
+
+switch ($params[0]) {
+
+    case 'home': //Muestra el Home
+        $controller = new HomeController();
+        $controller->showHome();
+        break;
+    case 'login':
+        $controller = new AuthController();
+        $controller->showLogin();
+        break;
+    case 'singup': 
+        $controller = new AuthController();
+        $controller->showSingup();
+        break;
+    case 'registro': 
+        $controller = new AuthController();
+        $controller->upUser();
+        break;
+    case 'about': 
+        $controller = new AboutController();
+        $controller->showAbout();
+        break;
+    case 'auth': 
+        $controller = new AuthController();
+        $controller->auth();
+        break;
+    case 'logOut': 
+        $controller = new AuthController();
+        $controller->logOut();
+        break;
+    default:
+    $controller = new ErrorController();
+    $controller->showError404($error);
+        break;
+}
