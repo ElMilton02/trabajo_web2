c1e36ad7720d1291b8ee621015f39552a5adf415
Milton Palucci <miltonpalucci02@gmail.com>
2023-10-17 21:05:01 -0300

    se añadieron todos los controllers y el helpers

---
+++ apps/helpers/AuthHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+class AuthHelper
+{
+
+    public static function init()
+    {
+        if (session_status() != PHP_SESSION_ACTIVE) {
+            session_start();
+        }
+    }
+
+    public static function login($userDb)
+    {
+        AuthHelper::init();
+        $_SESSION['USER_ROL'] = $userDb->rol;
+        $_SESSION['USER_NAME'] = $userDb->usuario;
+        $_SESSION['USER_ID'] = $userDb->id;
+    }
+
+    public static function logOut()
+    {
+        AuthHelper::init();
+        session_destroy();
+        header('Location: ' . BASE_URL . '');
+        die;
+    }
+
+    public static function verify($page = null)
+    {
+        AuthHelper::init();
+        if (!isset($_SESSION['USER_ID'])) {
+            $controller = new ErrorController();
+            $controller->showErrorNonUser('Para acceder al contenido, por favor inicia sesión',$page);
+        }
+    }
+}
